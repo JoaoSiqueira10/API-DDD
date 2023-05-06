a930b1125237aa5fdee08a4cc134e9462d85224c
Joao Guilherme <joao.guilherme100@hotmail.com>
2023-05-06 10:53:40 -0300

    Customizacao das Interface do Servico

--- API_DDD/Infrastructure/Repository/Repositories/RepositoryMessage.cs
+++ API_DDD/Infrastructure/Repository/Repositories/RepositoryMessage.cs
@@ -6,6 +6,8 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +21,14 @@ namespace Infrastructure.Repository.Repositories
         public RepositoryMessage()
         {
             _OptionsBuilder = new DbContextOptions<ContextBase>();
+        }
+
+        public async Task<List<Message>> ListarMessage(Expression<Func<Message, bool>> exMessage)
+        {
+            using (var banco = new ContextBase(_OptionsBuilder))
+            {
+                return await banco.Message.Where(exMessage).AsNoTracking().ToListAsync();
+            }
         }
     }
 }
--- API_DDD/WebAPIs/Controllers/MessageController.cs
+++ API_DDD/WebAPIs/Controllers/MessageController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Domain.Interfaces;
+using Domain.Interfaces.InterfaceService;
 using Entities.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -14,11 +15,13 @@ namespace WebAPIs.Controllers
     {
         private readonly IMapper _IMapper;
         private readonly IMessage _IMessage;
+        private readonly IServiceMessage _IServiceMessage;
 
-        public MessageController(IMapper IMapper, IMessage IMessage)
+        public MessageController(IMapper IMapper, IMessage IMessage, IServiceMessage IServiceMessage)
         {
             _IMapper = IMapper;
             _IMessage = IMessage;
+            _IServiceMessage = IServiceMessage;
         }
 
         [Authorize]
@@ -28,7 +31,8 @@ namespace WebAPIs.Controllers
         {
             message.UserId = await RetornarIdUsuarioLogado();
             var messageMap = _IMapper.Map<Message>(message);
-            await _IMessage.Add(messageMap);
+            //await _IMessage.Add(messageMap);
+            await _IServiceMessage.Adicionar(messageMap);
             return messageMap.Notificacoes;
         }
 
@@ -38,7 +42,8 @@ namespace WebAPIs.Controllers
         public async Task<List<Notifies>> Update(MessageViewModel message)
         {
             var messageMap = _IMapper.Map<Message>(message);
-            await _IMessage.Update(messageMap);
+            //await _IMessage.Update(messageMap);
+            await _IServiceMessage.Atualizar(messageMap);
             return messageMap.Notificacoes;
         }
 
@@ -68,6 +73,16 @@ namespace WebAPIs.Controllers
         public async Task<List<MessageViewModel>> List()
         {
             var mensagens = await _IMessage.List();
+            var messageMap = _IMapper.Map<List<MessageViewModel>>(mensagens);
+            return messageMap;
+        }
+
+        [Authorize]
+        [Produces("application/json")]
+        [HttpPost("/api/List")]
+        public async Task<List<MessageViewModel>> ListarMessageAtivas()
+        {
+            var mensagens = await _IServiceMessage.ListarMessageAtivas();
             var messageMap = _IMapper.Map<List<MessageViewModel>>(mensagens);
             return messageMap;
         }
